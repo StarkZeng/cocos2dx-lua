6b5705d40b79e134456c312f271a95579a8076e5
405939066@qq.com <405939066@qq.com>
2015-06-15 18:10:57 +0800

    modify lua engine

--- tests/lua-empty-test/src/game/EngineDefault.lua
+++ tests/lua-empty-test/src/game/EngineDefault.lua
@@ -1,13 +1,50 @@
 cc.FileUtils:getInstance():addSearchPath("src")
 cc.FileUtils:getInstance():addSearchPath("res")
+-- CC_USE_DEPRECATED_API = true
+require "cocos.init"
 
-require "game.EngineDefalut"
 
+
+-- cclog
+cclog = function(...)
+    print(string.format(...))
+end
+
+-- for CCLuaEngine traceback
+function __G__TRACKBACK__(msg)
+    cclog("----------------------------------------")
+    cclog("LUA ERROR: " .. tostring(msg) .. "\n")
+    cclog(debug.traceback())
+    cclog("----------------------------------------")
+end
+
+local function initCollectGarbage()
+    collectgarbage("setpause", 100)
+    collectgarbage("setstepmul", 5000)
+end
+
+local function initGLView()
+    local director = cc.Director:getInstance()
+    local glView = director:getOpenGLView()
+    if nil == glView then
+        glView = cc.GLViewImpl:create("Lua Empty Test")
+        director:setOpenGLView(glView)
+    end
+
+    director:setOpenGLView(glView)
+
+    glView:setDesignResolutionSize(480, 320, cc.ResolutionPolicy.NO_BORDER)
+
+    --turn on display FPS
+    director:setDisplayStats(true)
+
+    --set FPS. the default value is 1.0/60 if you don't call this
+    director:setAnimationInterval(1.0 / 60)
+end
 
 local function main()
     -- avoid memory leak
-    collectgarbage("setpause", 100)
-    collectgarbage("setstepmul", 5000)
+    
 
     initGLView()
 
